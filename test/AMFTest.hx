4ae809be8d588058a8799afe5047e5522a66bd14
sonygod <sonygodx@163.com>
2012-10-28 18:01:59 +0800

    and marco to write client call service method. see tink_lang wiki

--- test/AMFTest.hx
+++ test/AMFTest.hx
@@ -6,41 +6,88 @@ package ;
  */
 
 import haxe.remoting.AMFConnection;
-
+import flash.utils.ByteArray;
+import ICall;
+import tink.lang.Cls;
+import haxe.io.Bytes;
 /**
  * ...
- * @author Guntur Sarwohadi
+ * @author sonygod
  */
 
-class AMFTest
-{
+class AMFTest {
 
-       public static function main():Void
-       {
-               var url:String = "http://localhost:8080/index.n";
-               var c:AMFConnection = AMFConnection.urlConnect(url);
-               c.setErrorHandler(onError);
-               c.HelloService.hello.call(["one", "two"], onResult);
-			   
-			   var aa:Array<String> = ["1", "2"];
-			   c.HelloService.getLength.call([aa], onResult);
-			   
-			  
-			   
-			
-			   
-			   
-			   
-       }
+    public static function main():Void {
+        var url:String = "http://localhost:8080/index.n";
+        var c:AMFConnection = AMFConnection.urlConnect(url);
+        c.setErrorHandler(onError);
+         //basic call, don't got any tip from ide..
+        c.HelloService.hello.call(["one", "two"], onResult);
 
-       static private function onResult(r:Dynamic):Void
-       {
-               trace("result: " + r);
-       }
 
-       static private function onError(e:Dynamic):Void
-       {
-               trace("error: " + Std.string(e));
-       }
+        function onResult(r:Dynamic) {
+            trace("result: " + r);
+        }
 
+        function onError(e:Dynamic) {
+            trace("error: " + Std.string(e));
+        }
+
+
+        var target = {
+
+        onResult:onResult,
+        onError:onError,
+        AMF:c,
+
+        };
+        var f = new Forwarder(target);
+        //special call,got tips.
+        f.hello("one", "two");
+        f.getLength(["1", "2", "3"]);
+
+    }
+
+    static private function onResult(r:Dynamic):Void {
+        trace("Static result: " + r);
+    }
+
+    static private function onError(e:Dynamic):Void {
+        trace("Static error: " + Std.string(e));
+    }
+
+}
+typedef Hellox = {
+    function hello(x:String, y:String):String;
+    function getLength(data:Array < String>):Int;
+    function getObject(obj:Obj):String;
+    function getClass(p:Bytes):Dynamic;
+}
+
+typedef FwdTarget = {
+
+    function onError(e:Dynamic):Void;
+
+    function onResult(r:Dynamic):Void;
+
+    var AMF:AMFConnection;
+}
+
+
+class Forwarder implements Cls {
+    var fields:Hash<Dynamic> = new Hash<Dynamic>();
+    @:forward(!multiply) var target:FwdTarget;
+
+    @:forward function fwd2(HelloService:Hellox) {
+    get: fields.get($name),
+    set: fields.set($name, param),
+
+    call:target.AMF.resolve($id).resolve($name).call($args, target.onResult),
+    }
+
+
+    public function new(target) {
+    this.target = target;
+
+    }
 }
